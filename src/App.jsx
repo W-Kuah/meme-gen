4a0d2680ac71e016320293e48dfa6f6d2d58eae5
W-Kuah <45501552+W-Kuah@users.noreply.github.com>
2025-06-28 02:51:11 +1000

    tweak import

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import Header from './components/Header';
-import Body from './components/Body';
+import Header from './components/Header.jsx';
+import Body from './components/Body.jsx';
 import './App.css';
 
 function App() {
@@ -12,4 +12,4 @@ function App() {
   )
 }
 
-export default App
+export default App;
