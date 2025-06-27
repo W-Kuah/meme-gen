d3699fc5e8117ca640171ecc0356d1ad4ea4e0bc
W-Kuah <45501552+W-Kuah@users.noreply.github.com>
2025-06-28 03:24:16 +1000

    import tweak

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import Header from '/src/components/Header.jsx';
-import Body from '/src/components/Body.jsx';
+import Header from 'src/components/Header.jsx';
+import Body from 'src/components/Body.jsx';
 import './App.css';
 
 function App() {
